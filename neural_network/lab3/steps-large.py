0c34994409a29e01c5c4aefcc9aa8863db905f0e
pi1415926535 <makowskit2@student.mini.pw.edu.pl>
2020-04-11 18:12:23 +0200

    create final plots for lab3

--- neural_network/lab3/steps-large.py
+++ neural_network/lab3/steps-large.py
@@ -1,6 +1,6 @@
 from neural_network.activations import *
 from neural_network.data_manipulation import load_data
-from neural_network.plots import plot_measure_results_data
+from neural_network.plots import plot_measure_results_data, plot_data_1d_compare
 from neural_network.learn_network import learn_network
 
 # load dataset
@@ -10,13 +10,16 @@ x = train[:, 0:1]
 y = train[:, 1:2]
 
 # learn network without any momentum technique and show model
-mse = learn_network(x, y, [20], [sigmoid, linear], momentum_type='normal', eta=0.01, epochs=1, iterations=20)
+mse, base = learn_network(x, y, [20], [sigmoid, linear], momentum_type='normal', eta=0.01, epochs=1, iterations=100, plot=False, return_result=True)
 
 # learn network with momentum and show model
-mse_mom = learn_network(x, y, [20], [sigmoid, linear], momentum_type='momentum', lambda_momentum=0.9, eta=0.01, epochs=1, iterations=20)
+mse_mom, mom = learn_network(x, y, [20], [sigmoid, linear], momentum_type='momentum', lambda_momentum=0.9, eta=0.01, epochs=1, iterations=100, plot=False, return_result=True)
 
 # learn model with RMSProp and show model
-mse_rms = learn_network(x, y, [50, 50], [sigmoid, sigmoid, linear], momentum_type='RMSProp', beta=0.1, eta=0.1, epochs=1, iterations=20)
+mse_rms, rms = learn_network(x, y, [20], [sigmoid, linear], momentum_type='RMSProp', beta=0.01, eta=0.1, epochs=1, iterations=100, plot=False, return_result=True)
 
-# show error (mse) after each epoch
-plot_measure_results_data([mse, mse_mom, mse_rms])
+labels = ['No momentum', 'Momentum', 'RMSProp']
+
+# plot data and mse
+plot_data_1d_compare(x, y, [base, mom, rms], labels=["true"] + labels, title="Comparison of momentum learning")
+plot_measure_results_data([mse, mse_mom, mse_rms], labels=labels, title_ending=" for momentum learning", from_error=5)
