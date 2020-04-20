ed529e482dd3a16710591477126b1ae2c3f5712f
pi1415926535 <makowskit2@student.mini.pw.edu.pl>
2020-04-20 23:07:27 +0200

    add mexican hat example

--- kohonen/hexagon.py
+++ kohonen/hexagon.py
@@ -12,3 +12,13 @@ plot_point_map(data, classes, network, ['Class 0', 'Class 1', 'Class 2', 'Class 
 #
 # from neural_network.plots import plot_data_2d
 # plot_data_2d(data[:, :1], data[:, 1:], classes)
+
+#Mexican hat
+network = Kohonen(15, 15, 2, neighbour_param=0.8, distance_type="not_gauss_(its_enough)", t=1)
+network.learn_epochs(data, epochs=50)
+plot_point_map(data, classes, network, ['Class 0', 'Class 1', 'Class 2', 'Class 3', 'Class 4', 'Class 5'], colors=['r', 'g', 'b', 'y', 'c', 'k'])
+
+
+network = Kohonen(10, 10, 2, neighbour_param=0.1, distance_type="not_gauss_(its_enough)", t=0.01)
+network.learn_epochs(data, epochs=50)
+plot_point_map(data, classes, network, ['Class 0', 'Class 1', 'Class 2', 'Class 3', 'Class 4', 'Class 5'], colors=['r', 'g', 'b', 'y', 'c', 'k'])
